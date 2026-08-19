cc6da30c722fdc075558ec1d2ab827d6f4038a18
Nobrain11 <chainpulseink@gmail.com>
2026-08-19 07:21:03 +0100

    Add constants for RPC URL and addresses

---
+++ src/lib/constants.ts
@@ -0,0 +1,4 @@
+export const RPC_URL = process.env.NEXT_PUBLIC_RPC_URL || 'https://rpc.mainnet.chain.robinhood.com';
+export const WETH_ADDRESS = '0x0Bd7D308f8E1639FAb988df18A8011f41EAcAD73';
+export const BAGS_LENS_ADDRESS = '0xC82Db941dAf90B754aecb5F7D14c683dc608d595';
+export const UNIVERSAL_ROUTER = '0x8876789976dEcBfCbBbe364623C63652db8C0904';
