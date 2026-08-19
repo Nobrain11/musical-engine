34fca3339b9c5b958e07946c6a98a414877888c3
Nobrain11 <chainpulseink@gmail.com>
2026-08-19 08:23:51 +0100

    Add chain configuration constants

---
+++ src/lib/chain.ts
@@ -0,0 +1,9 @@
+export const CHAIN_ID = 4663;
+export const RPC_URL = process.env.NEXT_PUBLIC_RPC_URL || 'https://rpc.mainnet.chain.robinhood.com';
+export const EXPLORER = 'https://robinhoodchain.blockscout.com';
+
+// Bags Protocol addresses
+export const WETH_ADDRESS = '0x0Bd7D308f8E1639FAb988df18A8011f41EAcAD73';
+export const BAGS_FACTORY = '0xe8Cc4431adF8b5A847C113EF0c6af9043219Cb37';
+export const BAGS_LENS_ADDRESS = '0xC82Db941dAf90B754aecb5F7D14c683dc608d595';
+export const UNIVERSAL_ROUTER = '0x8876789976dEcBfCbBbe364623C63652db8C0904';
